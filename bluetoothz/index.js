e2636eb27f61c74253c1ca54400fc142e20f4f5b
Samuele Scatena <samuele@MBP-di-Samuele.fritz.box>
2023-04-02 14:59:33 +0200

    dd

--- bluetoothz/index.js
+++ bluetoothz/index.js
@@ -4,26 +4,61 @@ import {
   NativeModules,
 } from 'react-native';
 
-export function startScan() {
-                            console.log('pipo');
+// import our BLE native module
+const {BluetoothZ} = NativeModules;
+// adding listner, to receive ALL ble events
+const eventEmitter = new NativeEventEmitter(BluetoothZ);
+// Operation type
+const OperationType = {StartScan: 0, StopScan};
+// operation queue, to schedule app's requests to Ble
+class OperationQueue {
+  constructor() {
+    this.items = {};
+    this.frontIndex = 0;
+    this.backIndex = 0;
+  }
+  enqueue(item) {
+    this.items[this.backIndex] = item;
+    this.backIndex++;
+    // return item + ' inserted'
+  }
+  dequeue() {
+    const item = this.items[this.frontIndex];
+    delete this.items[this.frontIndex];
+    this.frontIndex++;
+    return item;
+  }
+  peek() {
+    return this.items[this.frontIndex];
+  }
+  get printQueue() {
+    return this.items;
+  }
+}
+
+const queue = new OperationQueue();
+
+const eventListener = event => {
+  // callback(event as BleDevice);
+};
+
+export function startScan(callback) {
+  console.log('pipo');
+}
+
+export function stopScan(callback) {
+  console.log('stopScan');
 }
 
 // export namespace BluetoothService {
-//   const eventEmitter = new NativeEventEmitter(NativeModules.BluetoothBridge);
 
 //   export interface BleDevice {
 //     name: string;
 //   }
 
-//   export function startScanForDevices(
-//     callback: (device: BleDevice) => void,
-//   ): EmitterSubscription | undefined {
-//     const eventListener = (event: any) => {
-//       callback(event as BleDevice);
-//     };
+//   export function startScanForDevices(callback: (device: BleDevice) => void ): EmitterSubscription | undefined {
 
-//     const {BluetoothBridge} = NativeModules;
-//     BluetoothBridge.startScan();
+//     BluetoothZ.startScan();
 
 //     const sub = eventEmitter!.addListener('Device', eventListener);
 
