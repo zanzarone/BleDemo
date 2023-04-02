3dfc8d9cc4902db0638ec95c7f62e2c23a913833
Samuele Scatena <samuele@MBP-di-Samuele.fritz.box>
2023-04-02 22:47:34 +0200

    ddd

--- bluetoothz/index.js
+++ bluetoothz/index.js
@@ -6,49 +6,80 @@ import {
 
 // import our BLE native module
 const {BluetoothZ} = NativeModules;
+
 // adding listner, to receive ALL ble events
 const eventEmitter = new NativeEventEmitter(BluetoothZ);
-// Operation type
-const OperationType = {StartScan: 0, StopScan};
+
+const OperationType = {
+  StartScan: 0,
+  StopScan,
+};
+
 // operation queue, to schedule app's requests to Ble
 class OperationQueue {
   constructor() {
-    this.items = {};
-    this.frontIndex = 0;
-    this.backIndex = 0;
+    // Create an empty array of commands
+    this.queue = [];
+    // We're inactive to begin with
+    this.active = false;
   }
-  enqueue(item) {
-    this.items[this.backIndex] = item;
-    this.backIndex++;
-    // return item + ' inserted'
+
+  // Method for adding command chain to the queue
+  place(command, callback) {
+    // Push the command onto the command array
+    this.queue.push({command, callback});
+    // If we're currently inactive, start processing
+    if (!this.active) this.next();
   }
-  dequeue() {
-    const item = this.items[this.frontIndex];
-    delete this.items[this.frontIndex];
-    this.frontIndex++;
-    return item;
+
+  // Method for calling the next command chain in the array
+  next() {
+    // If this is the end of the queue
+    if (!this.queue.length) {
+      // We're no longer active
+      this.active = false;
+      // Stop execution
+      return;
+    }
+    // Grab the next command
+    const command = this.queue.shift();
+    // We're active
+    this.active = true;
+    // Call the command
+    command();
+    this.next();
   }
-  peek() {
-    return this.items[this.frontIndex];
+
+  //Clearing queue
+  clear() {
+    this.queue.length = 0;
+    this.active = false;
   }
-  get printQueue() {
-    return this.items;
+}
+// const queue = new OperationQueue();
+
+class BluetoothService {
+  constructor() {
+    this.commands = new OperationQueue();
+  }
+
+  startScan() {
+    const eventListener = event => {
+      // callback(event as BleDevice);
+    };
+    this.commands.place();
   }
 }
 
-const queue = new OperationQueue();
+module.exports = BluetoothService;
 
-const eventListener = event => {
-  // callback(event as BleDevice);
-};
+// export function startScan(callback) {
+//   console.log('pipo');
+// }
 
-export function startScan(callback) {
-  console.log('pipo');
-}
-
-export function stopScan(callback) {
-  console.log('stopScan');
-}
+// export function stopScan(callback) {
+//   console.log('stopScan');
+// }
 
 // export namespace BluetoothService {
 
